74777a98bb41fb6960f4eed3eba8b2e830712f7b
Daniel <danielnaveda88@gmail.com>
2019-03-04 15:42:11 +0100

    add dgraph support

--- server/index.js
+++ server/index.js
@@ -1,29 +1,28 @@
 const express = require('express');
 const path = require('path');
+const dgraph = require("dgraph-js-http");
 var proxy = require('http-proxy-middleware')
 
-// To send custom graphql request
-// const { request } = require('graphql-request');
+// const clientStub = new dgraph.DgraphClientStub("http://localhost:8080");
+// const dgraphClient = new dgraph.DgraphClient(clientStub);
+// const schema = "name: string @index(exact) .";
+// dgraphClient.alter({ schema: schema });
 
-// const query = `{
-//   User {
-//     name
-//   }
-// }`
-
-// request('http://localhost:7474/graphql/', query).then(data =>
-//   console.log(data)
-// )
 
 // Initialize the app
 const app = express();
 
+app.get('/api/rest/v1', function (req, res) {
+  res.send(JSON.stringify({ version: "1.0.1" }));
+});
+
 // Use the react build folder to serve those static files
 app.use(express.static(path.join(__dirname, '../', 'build')));
 
-app.use('/graphql', proxy(
+app.use('/query', proxy(
   {
-    target: 'http://localhost:7474',
+    // target: 'http://localhost:8080',
+    target: 'http://server:8080',
     changeOrigin: true,
     logLevel: 'debug'
   }
